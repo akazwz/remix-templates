91df358d93419e937823179e63ee86ab6b05db24
akazwz <50396286+akazwz@users.noreply.github.com>
2024-02-04 23:19:50 +0800

    init

---
+++ vite-tw/README.md
@@ -0,0 +1,7 @@
+# Welcome to Remix + Vite + Tailwindcss + Prettier!
+
+## how to use
+
+```sh
+pnpm create remix --template akazwz/remix-templates/vite-tw
+```
---
+++ vite-tw/app/routes/_index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="font-bold flex h-svh justify-center items-center">
+      remix vite tailwindcss starter
+    </div>
+  );
+}
